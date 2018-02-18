85d9fa32d9eedcd0623ae2a0ec4aa231692ecd7d
Clare <chblackburne@gmail.com>
2018-02-18 11:37:53 +0000

    Installs semantic-ui and stylesheet

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'semantic-ui-css/semantic.min.css';
+
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>  
+  </BrowserRouter>
   , document.getElementById('root'));
 registerServiceWorker();
