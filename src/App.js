f50d24300087eeac69e086ed4fefdb3e04f67644
Clare <chblackburne@gmail.com>
2018-02-18 11:34:31 +0000

    Sets up homepage and login routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Homepage from './components/pages/Homepage';
+import Loginpage from './components/pages/Loginpage';
 
 
-const App = () => <div>hello world</div>
+const App = () => <div>
+  <Route path="/" exact component={Homepage}></Route>
+  <Route path="/login" exact component={Loginpage}></Route>
+
+</div>
 
 export default App;
---
+++ src/components/pages/Loginpage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Loginpage = () => (
+  <div>
+    <h1>Log in page </h1>
+
+  </div>
+);
+
+export default Loginpage;
