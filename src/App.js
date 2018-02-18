26cd8823aa0f398eacd623f8cb33339a6834687b
Clare <chblackburne@gmail.com>
2018-02-18 18:32:47 +0000

    Adds dashboard route and confirm email message

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Homepage from './components/pages/Homepage';
 import Loginpage from './components/pages/Loginpage';
+import Dashboardpage from './components/pages/Dashboardpage';
 
 
 const App = () => <div className="ui container">
   <Route path="/" exact component={Homepage}></Route>
   <Route path="/login" exact component={Loginpage}></Route>
+  <Route path="/dashboard" exact component={Dashboardpage}></Route>
 
 </div>
 
