a68b57886a1c02d26ed415e13c133df34318f003
Clare <chblackburne@gmail.com>
2018-02-18 18:41:21 +0000

    Adds route protection using react-router-dom's Redirect if user not authenticated by token

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { Route } from 'react-router-dom';
 import Homepage from './components/pages/Homepage';
 import Loginpage from './components/pages/Loginpage';
 import Dashboardpage from './components/pages/Dashboardpage';
+import UserRoute from './components/routes/UserRoute';
 
 
 const App = () => <div className="ui container">
   <Route path="/" exact component={Homepage}></Route>
   <Route path="/login" exact component={Loginpage}></Route>
-  <Route path="/dashboard" exact component={Dashboardpage}></Route>
+  <UserRoute path="/dashboard" exact component={Dashboardpage}></Route>
 
 </div>
 
