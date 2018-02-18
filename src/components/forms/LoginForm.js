5a03ae903236951e5a837a1f425bfbd2a506fe8d
Clare <chblackburne@gmail.com>
2018-02-18 12:00:37 +0000

    Creates basic validator function

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,19 +1,61 @@
 import React from 'react';
 import { Button, Form } from 'semantic-ui-react';
+import Validator from 'validator';
 
 class LoginForm extends React.Component {
   state = {
-    data: {},
+    data: {
+      email: '',
+      password: ''
+    },
     loading: false,
     errors: {}
+  };
+//only suited for text input
+  onChange = e =>
+    this.setState({
+      data: {
+        //saves what's already there
+        ...this.state.data,
+        //adds input from user
+      [e.target.name]: e.target.value
+      }
+    });
+
+  onSubmit = () => {
+    const errors = this.validate(this.state.data);
+    this.setState({ errors }); //if this object is empty, there aren't errors!
+  }
+  validate = (data) => {
+    const errors = {};
+    if(!Validator.isEmail(data.email)) errors.email = "Invalid email";
+    if(!data.password) errors.password = "Can't be blank";
+
+    return errors; //if errors object is empty, proceed
   }
 
   render(){
+    const { data } = this.state;
     return(
-      <Form>
+      <Form onSubmit={this.onSubmit}>
+        <Form.Field>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" placeholder="example@example.com"
+          value={data.email}
+          onChange={this.onChange}/>
+        </Form.Field>
+        <Form.Field>
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" placeholder="Make it secure"
+          value={data.password}
+          onChange={this.onChange}/>
+        </Form.Field>
         <Button primary>Login</Button>
       </Form>
     );
   }
 }
+LoginForm.propTypes = {
+  submit: PropTypes.func.isRequired
+};
 export default LoginForm;
