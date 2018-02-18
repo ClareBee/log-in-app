0547835c48c9c8fee1222d262383b67fe7545a4f
Clare <chblackburne@gmail.com>
2018-02-18 13:34:35 +0000

    Sets up Redux

---
+++ src/rootReducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+
+//reducers take state and return new state - combined into rootreducer for the whole tree
+export default combineReducers({
+  user: () => ({})
+})
