dabd6e5fe0ce06b80d80144cb3767856774392e3
Clare <chblackburne@gmail.com>
2018-02-18 11:27:28 +0000

    Adds react router

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
       "react",
       "jsx-a11y",
       "import"
-    ]
+    ],
     // install the following packages:
 // yarn add --dev eslint prettier eslint-config-airbnb@^15.0.1 eslint-config-prettier eslint-plugin-prettier eslint-plugin-react eslint-plugin-import eslint-plugin-jsx-a11y@^5.1.1
 
@@ -23,6 +23,7 @@ module.exports = {
     "node": true
   },
   "rules": {
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "prettier/prettier": "error"
 }
 };
