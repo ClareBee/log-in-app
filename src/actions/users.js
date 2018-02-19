102eb698fb48a9d575f84dd328886115d31ec0f1
Clare <chblackburne@gmail.com>
2018-02-19 19:49:31 +0000

    Fixes bug - adds JWT to localstorage to stay logged in when page refreshed

--- src/actions/users.js
+++ src/actions/users.js
@@ -2,4 +2,8 @@ import api from '../api';
 import { userLoggedIn } from './auth';
 //thunk action
 export const signup = (data) => (dispatch) =>
-api.user.signup(data).then(user => dispatch(userLoggedIn(user)))
+api.user.signup(data).then(user => {
+  //adding to local storage to keep signed in if page refreshed
+  localStorage.bookwormJWT = user.token,
+  dispatch(userLoggedIn(user));
+});
