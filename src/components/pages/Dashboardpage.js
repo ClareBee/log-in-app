62bef5f1e6e93f55074a4003abdc92bb3242aed5
Clare <chblackburne@gmail.com>
2018-03-02 18:51:03 +0000

    Adds books reducer

--- src/components/pages/Dashboardpage.js
+++ src/components/pages/Dashboardpage.js
@@ -7,14 +7,19 @@ import ConfirmEmailMessage from '../messages/ConfirmEmailMessage';
 const Dashboardpage = ({ isConfirmed }) => (
   <div>
     {!isConfirmed && <ConfirmEmailMessage />}
+    {books.length === 0 && <AddBookCallToAction />}
   </div>
 );
 Dashboardpage.propTypes = {
-  isConfirmed: PropTypes.bool.isRequired
+  isConfirmed: PropTypes.bool.isRequired,
+  books: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired
+  }).isRequired).isRequired
 }
 function mapStateToProps(state){
   return {
-    isConfirmed: !!state.user.confirmed
+    isConfirmed: !!state.user.confirmed,
+    books:
   }
 }
 export default connect(mapStateToProps)(Dashboardpage);
--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import user from './reducers/user';
+import books from './reducers/books';
 //reducers take state and return new state - combined into rootreducer for the whole tree
 export default combineReducers({
-  user
+  user,
+  books
 })
