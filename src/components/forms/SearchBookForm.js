46fd1e380af56e5f29031b05d495a134f93275c3
Clare <chblackburne@gmail.com>
2018-03-02 19:14:09 +0000

    Adds search form basics

---
+++ src/components/forms/SearchBookForm.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Form, Dropdown } from 'semantic-ui-react';
+
+class SearchBookForm extends React.Component {
+  state = {
+    query: '',
+    loading: false,
+    options: [{
+      key: 1,
+      value: 1,
+      text: 'some book'
+    },
+    {
+      key: 2,
+      value: 2,
+      text: 'another book'
+    }],
+    books: {}
+  }
+  fetchOptions = () => {
+    if(!this.state.query) return;
+    this.setState({
+      loading: true
+    });
+  }
+
+  onSearchChange = (e, data) => {
+    clearTimeout(this.timer);
+    this.setState({
+      query: data
+    });
+    this.timer = setTimeout(this.fetchOptions, 1000);
+  }
+  
+  render(){
+    return(
+      <Form>
+        <Dropdown search fluid placeholder="Search for book by title" value={this.state.query} onSearchChange={this.onSearchChange}></Dropdown>
+      </Form>
+    )
+  }
+}
+export default SearchBookForm;
